2af276d7b8a88ec8bc214426c71c52bb85470a68
Uchannel92 <uchenna_attah@hotmail.com>
2024-03-10 13:24:29 +0000

    added styling to format the view of each instance object created as per issue#2

--- sneakers.py
+++ sneakers.py
@@ -24,11 +24,25 @@ class Footwear:
     def __repr__(self):
         shoe = f"Footwear({self.colour}, {self.brand}, {self.style})"
         return shoe
+    
+
+    def __str__(self):
+
+        shoe = ''
+        shoe += f"\n{'='*50}\n" # header
+        shoe += f"Colour: {self.colour}\n" # item colour
+        shoe += f"{'-'*50}\n"
+        shoe += f"Brand: {self.brand}\n" # item brand
+        shoe += f"{'-'*50}\n"
+        shoe += f"Style: {self.style}\n" # item style
+        shoe += f"{'='*50}\n" # footer
+
+        return shoe
 
 test_closet = ShoeContainer()
 test_shoe = Footwear("Black", "Clarks", "Boot")
 second_test_shoe = Footwear("Silver", "Nike", "Running")
-print(test_shoe, second_test_shoe)
+print(test_shoe)
 print(test_closet.show_items())
 test_closet.add_shoe(test_shoe)
 print(test_closet.show_items())
